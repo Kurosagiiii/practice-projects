35503af183d1a3464beb5ff765a4865a417d6cee
Kurosagiiii <your-github-email@example.com>
2025-09-20 15:48:03 +0600

    v2

---
+++ Insta page practice/Practice 1/script.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+let name = prompt("What's your name? ");
+console.log("Hello " + name);
+
+let food = prompt("What's your favourite food? ");
+console.log("What a coincidence I like " + food + " too.");
+
+let age = prompt("Boyosh koto lol ");
+console.log("So you're " + age + "pfft buira beta");
+
+let location = prompt ("bari koi ");
+console.log("hmm " + location + "naki? chini na lol");
+
+console.log("adios");
