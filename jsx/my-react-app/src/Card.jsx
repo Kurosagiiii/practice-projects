997f993907014c2136a57f6001c476284bfbe61d
Kurosagiiii <your-github-email@example.com>
2025-09-24 13:45:30 +0600

    test

---
+++ jsx/my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card (){
+    
+    return(
+        <div className="card">
+            <img classname="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgViLr1lGxRzUWXLQDeN9mAhweN442MdNLSA&s/150" alt="profile picture"></img>
+            <h2 classname="card-title">Tempest Kurosagi</h2>
+            <p classname="card-text">Learing Codes and UI Designing</p>
+        </div>
+    );
+}
+
+export default Card
